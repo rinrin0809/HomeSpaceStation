d805f5a681451d099887a2c36edd61a379df66cd
xR1n <105471170+xR1n@users.noreply.github.com>
2024-10-08 10:37:16 +0900

    playerの上下左右移動実装

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    // 移動
+    public float moveSpeed; // 移動速度
+    private float horizontal; // x
+    private float vertical; // y
+
+    [SerializeField]
+    private Rigidbody2D rb;
+    private Vector3 moveDir;
+
+    // アニメーション
+    [SerializeField]
+    private Animator animatior;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 仮移動
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        // 斜め移動しないようにする
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+        moveDir = new Vector3(horizontal, vertical).normalized;
+    }
+    // 一定時間毎に呼ばれる関数
+    void FixedUpdate()
+    {
+        //rigidbody2d.velocity = moveDir * moveSpeed * Time.deltaTime;
+        rb.velocity = moveDir * moveSpeed * Time.fixedDeltaTime;
+
+    }
+
+}
