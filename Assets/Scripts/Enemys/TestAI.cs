d7e2e656e6e8aa37a1a3ee8880fc737d9f960685
Rinrin0809 <pingjingz71@gmail.com>
2024-10-09 09:32:26 +0900

    enemytest1

--- Assets/Scripts/Enemys/TestAI.cs
+++ Assets/Scripts/Enemys/TestAI.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class TestAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 3f;
+    public Transform target;
+
+    private Vector2 newPosition;
+
+    private void Awake()
     {
-        
+        newPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            float threshold = .1f;
+
+            // Only calculate new position if we are under the "threshold"
+            if (Vector2.Distance(transform.position, newPosition) < threshold)
+            {
+                newPosition = target.position - transform.position;
+
+                if (Mathf.Abs(newPosition.x) > Mathf.Abs(newPosition.y))
+                {
+                    newPosition.x = target.position.x;
+                    newPosition.y = transform.position.y;
+                }
+                else
+                {
+                    newPosition.x = transform.position.x;
+                    newPosition.y = target.position.y;
+                }
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, newPosition, step);
+        }
     }
 }
