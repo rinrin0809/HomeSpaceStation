7dedc5a6355c472f68cb30cf8ef6e230f0d2150d
Rinrin0809 <pingjingz71@gmail.com>
2024-11-21 17:50:48 +0900

    Door完成

--- Assets/Scripts/Gimic/Door.cs
+++ Assets/Scripts/Gimic/Door.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour
-{
-    private Player player;
+//public class Door : MonoBehaviour
+//{
+//    private Player player;
 
-    [SerializeField]
-    public string ItemName = "";
-    void Start()
-    {
-        player = FindObjectOfType<Player>();
-    }
+//    [SerializeField]
+//    public string ItemName = "";
+//    void Start()
+//    {
+//        player = FindObjectOfType<Player>();
+//    }
 
-    void Update()
-    {
-        if (player.GimicHitFlg)
-        {
-            for (int i = 0; i < player.GetInventory().GetSize(); i++)
-            {
-                foreach (var item in player.GetInventory().GetCurrentInventoryState())
-                {
-                    // インベントリのアイテムを取得して、ItemFlg をチェック
-                    if (item.Value.item.Name == ItemName)
-                    {
-                        gameObject.SetActive(false);
-                        break; // 一つ見つかったらループを抜ける
-                    }
-                }
-            }
-        }
-    }
-}
+//    void Update()
+//    {
+//        if (player.GimicHitFlg)
+//        {
+//            for (int i = 0; i < player.GetInventory().GetSize(); i++)
+//            {
+//                foreach (var item in player.GetInventory().GetCurrentInventoryState())
+//                {
+//                    // インベントリのアイテムを取得して、ItemFlg をチェック
+//                    if (item.Value.item.Name == ItemName)
+//                    {
+//                        gameObject.SetActive(false);
+//                        break; // 一つ見つかったらループを抜ける
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
--- Assets/Scripts/Gimic/SceneTransitions.cs
+++ Assets/Scripts/Gimic/SceneTransitions.cs
@@ -8,27 +8,61 @@ public class SceneTransitions : MonoBehaviour
     public Vector2 playerPosition;    //シーン遷移後のプレイヤー位置
     public VectorValue playerStorage; //プレイヤーの位置を保存
 
+    private Player player;
+
+    [SerializeField]
+    public string ItemName = "";
+    // 鍵が必要かどうかを判別するフラグ
+    public bool requiresKey = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+
     //シーン遷移
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger)
         {
-            //名前が悪いけどstarPositionに移動するフラグをtrue
-            if (!LoadManager.Instance)
+            // 鍵が必要かどうかチェック
+            if (!requiresKey || (requiresKey && HasRequiredItem()))
             {
-                LoadManager.Instance.NewGamePushFlg = true;
-            }
-            Debug.Log(LoadManager.Instance.NewGamePushFlg);
-            //// プレイヤーの向きを保存
-            //Player player = collision.GetComponent<Player>();
-            //if (player != null)
-            //{
-            //    player.SavePlayerRotation(); // プレイヤーの向きを保存
-            //}
+                //名前が悪いけどstarPositionに移動するフラグをtrue
+                if (!LoadManager.Instance)
+                {
+                    LoadManager.Instance.NewGamePushFlg = true;
+                }
+                Debug.Log(LoadManager.Instance.NewGamePushFlg);
 
-            // プレイヤーの位置を保存し、シーンを切り替え
-            playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+                // プレイヤーの位置を保存し、シーンを切り替え
+                playerStorage.initialValue = playerPosition;
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.Log("鍵を持っていないため、シーン遷移できません。");
+            }
         }
+    }
+
+    // 必要なアイテムを持っているか確認
+    private bool HasRequiredItem()
+    {
+        if (player != null)
+        {
+            for (int i = 0; i < player.GetInventory().GetSize(); i++)
+            {
+                foreach (var item in player.GetInventory().GetCurrentInventoryState())
+                {
+                    if (item.Value.item.Name == ItemName)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
