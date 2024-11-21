7799147714d33822f81f3705158081fa1d9452c0
xR1n <105471170+xR1n@users.noreply.github.com>
2024-11-21 14:00:33 +0900

    キャラデータの名前変更

--- Assets/Scripts/TalkEvent/CharacterData.cs
+++ Assets/Scripts/TalkEvent/CharacterData.cs
@@ -20,10 +20,12 @@ public class CharacterPost
 {
     public enum Post 
     {
-        protagonist,  // 主人公
-        protagonistgarlfrend, // 主人公の彼女
+        [Header("主人公")]
+        syujinkou,  // 主人公
+        syujinkou_gal, // 主人公の彼女
         frendboy, // 主人公の友達
         frendGarl, // 友達の彼女
+        okami,   // 女将
     }
 
 }
