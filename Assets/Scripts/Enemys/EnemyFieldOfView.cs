1f3b4cc8d3f532805073674dd627ab5e551f97cc
Rinrin0809 <pingjingz71@gmail.com>
2024-11-28 11:46:57 +0900

    適しあ

---
+++ Assets/Scripts/Enemys/EnemyFieldOfView.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFieldOfView : MonoBehaviour
+{
+    public float radious = 5;
+    [Range(1, 360)] public float angle = 45f;
+    public LayerMask targetLayer;
+    public LayerMask obstructionLayer;
+
+    public GameObject playerRef;
+
+    public bool CanSeePlayer { get; private set; }
+
+    private float fovchecktime = 0.2f;
+    private int half = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVCheck());
+    }
+
+    //”»’è
+    private IEnumerator FOVCheck()
+    {
+        WaitForSeconds wait = new WaitForSeconds(fovchecktime);
+
+        while(true)
+        {
+            yield return wait;
+            FOV();
+        }
+    }
+
+    //Ž‹–ì
+    private void FOV()
+    {
+        Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, radious, targetLayer);
+
+        if (rangeCheck.Length > 0)
+        {
+            Transform target = rangeCheck[0].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, directionToTarget) < angle / half)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionLayer))
+                {
+                    CanSeePlayer = true;
+                }
+                else
+                {
+                    CanSeePlayer = false;
+                }
+            }
+            else
+            {
+                CanSeePlayer = false;
+            }
+        }
+        else if (CanSeePlayer)
+        {
+            CanSeePlayer = false;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, radious);
+
+        Vector3 angle01 = DirectionFromAngle(-transform.eulerAngles.z, -angle / half);
+        Vector3 angle02 = DirectionFromAngle(-transform.eulerAngles.z, angle / half);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + angle01 * radious);
+        Gizmos.DrawLine(transform.position, transform.position + angle02 * radious);
+
+        if(CanSeePlayer)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, playerRef.transform.position);
+        }
+    }
+
+    private Vector2 DirectionFromAngle(float eulerY,float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
