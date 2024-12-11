baae8fffe772627d446902ce2e3edab0215a27a8
卓也 牧田 <2210268@ecc.ac.jp>
2024-12-11 11:01:02 +0900

    ミニゲームの表示・非表示

---
+++ Assets/Scripts/ActiveChange.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveChange : MonoBehaviour
+{
+    //アクティブ化を切り替えるオブジェクト
+    [SerializeField] private GameObject ActiveObject;
+    //↑がONの時にOFFにしたいオブジェクト
+    [SerializeField] private GameObject InverseActiveObject;
+
+    //アクティブ化の変更をするかのフラグ
+    [SerializeField] private bool ChangeFlg = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //フラグがtrueの時
+        if(ChangeFlg) Change();
+    }
+
+    public void Change()
+    {
+        if(ActiveObject.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                ActiveObject.SetActive(false);
+                if(InverseActiveObject != null) InverseActiveObject.SetActive(true);
+            }
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                ActiveObject.SetActive(true);
+                if (InverseActiveObject != null) InverseActiveObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ButtonChange()
+    {
+        if (ActiveObject.activeSelf)
+        {
+            this.gameObject.SetActive(false);
+            ActiveObject.SetActive(false);
+            if (InverseActiveObject != null) InverseActiveObject.SetActive(true);
+        }
+
+        else
+        {
+            ActiveObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Icon/MiniGame/MiniGameIcon.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGameIcon : MonoBehaviour
+{
+    //横移動時のフラグ
+    [SerializeField] private bool SideFlg;
+    //UIの位置
+    RectTransform RectTransformIns;
+    //アイコン（矢印）の移動
+    IconMove IconMoveIns;
+    //メニューの管理クラス
+    private MenuManager MenuManagerIns;
+    //ボタンの配列
+    public Button[] Buttons;
+
+    void Start()
+    {
+        //UIの位置取得
+        RectTransformIns = gameObject.GetComponent<RectTransform>();
+        //アイコンの移動取得
+        IconMoveIns = gameObject.GetComponent<IconMove>();
+        // MenuManager の取得
+        if (MenuManagerIns == null)
+        {
+            MenuManagerIns = Object.FindAnyObjectByType<MenuManager>(); // 自動取得
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移動処理
+        RectTransformIns.position = IconMoveIns.Move(SideFlg, RectTransformIns.position);
+        //押された時の処理
+        OnClick();
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            IconMoveIns.ResetNum();
+        }
+    }
+
+    //押された時の処理
+    void OnClick()
+    {
+        // スペースキーが押されたか確認 (KeyCode.Return はエンターキー)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 横移動の場合
+            if (SideFlg)
+            {
+                int SideNum = IconMoveIns.GetSideNum();
+                // ボタンが設定されている場合、ボタンのonClickイベントを呼び出す
+                if (Buttons[SideNum] != null)
+                {
+                    IconMoveIns.ResetNum();
+                    Buttons[SideNum].onClick.Invoke(); // ボタンのクリックイベントを呼び出す
+                }
+            }
+
+            else
+            {
+                int LengthNum = IconMoveIns.GetLengthNum();
+                // ボタンが設定されている場合、ボタンのonClickイベントを呼び出す
+                if (Buttons[LengthNum] != null)
+                {
+                    IconMoveIns.ResetNum();
+                    Buttons[LengthNum].onClick.Invoke(); // ボタンのクリックイベントを呼び出す
+                }
+            }
+        }
+    }
+}
