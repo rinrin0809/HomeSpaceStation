6161b8daad8361865c5c9e2de27bba3da4229e40
卓也 牧田 <2210268@ecc.ac.jp>
2025-01-10 16:31:22 +0900

    MメニューのUIを徐々に透明にするように変更

--- Assets/Scripts/Icon/IconOff.cs
+++ Assets/Scripts/Icon/IconOff.cs
@@ -1,20 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IconOff : MonoBehaviour
 {
+    //消すオブジェクト
     [SerializeField] private GameObject GameObj;
-
+    //消すオブジェクトの色
+    [SerializeField] private Color ObjColor;
+    //消すオブジェクトのイメージ
+    Image ObjImage;
+    //透明度
+    private float Alpha = 1.0f;
+    //徐々に消す時間
+    private float AlphaTime = 2.0f;
     void Start()
     {
-        
+        //初期化
+        ObjColor = new Color(1.0f,1.0f,1.0f,1.0f);
+        //消すオブジェクトのイメージ取得
+        ObjImage = GameObj.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Player.Instance.GetisMoving() || MenuManager.Instance.GetOpenMenuFlg())
+        //透明度を更新
+        ObjImage.color = ObjColor;
+
+        //プレイヤーが動いている時またはメニューを開いている時
+        if (Player.Instance.GetisMoving())
+        {
+            if (ObjColor.a < 0.0f)
+            {
+                ObjColor.a = 0.0f;
+            }
+
+            else
+            {
+                ObjColor.a -= AlphaTime * Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if(ObjColor.a > 1.0f)
+            {
+                ObjColor.a = 1.0f;
+            }
+
+            else
+            {
+                ObjColor.a += AlphaTime * Time.deltaTime;
+            }
+        }
+
+        if (MenuManager.Instance.GetOpenMenuFlg())
         {
             GameObj.SetActive(false);
         }
