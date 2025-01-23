e235dbcdf6052ccc349b2a78e763dcce6cb64e5e
卓也 牧田 <2210268@ecc.ac.jp>
2025-01-23 11:06:23 +0900

    フェードインのバグ修正とシーン遷移の判定をプレハブ化

---
+++ Assets/Scripts/SceneChanger/SceneChanger.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private bool OnFlg = false;
+
+    [SerializeField]
+    string NextSceneName;
+
+    // フェードアウト
+    private FadeOutSceneLoader fadeOutSceneLoader;
+
+    void Start()
+    {
+        fadeOutSceneLoader = FindObjectOfType<FadeOutSceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ゲームオブジェクトの有効化
+        On();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            OnFlg = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            OnFlg = false;
+        }
+    }
+
+    //ゲームオブジェクトの有効化
+    private void On()
+    {
+        if (OnFlg && Input.GetKeyDown(KeyCode.Space))
+        {
+            Player.Instance.UpdateFlg = false;
+            fadeOutSceneLoader.NewGameCallCoroutine(NextSceneName);
+        }
+
+        else if (!OnFlg || OnFlg && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Player.Instance.UpdateFlg = true;
+        }
+    }
+}
